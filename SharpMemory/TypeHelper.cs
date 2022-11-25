08e3531072980b527e63a413ec585d6ac59b9bdf
Hex <119023047+SleepyHex@users.noreply.github.com>
2022-11-25 17:54:34 -0500

    Update TypeHelper.cs

--- SharpMemory/TypeHelper.cs
+++ SharpMemory/TypeHelper.cs
@@ -3,19 +3,18 @@ internal class TypeHelper
 {
     public static bool IsBasicType(Type type)
     {
-        if(type == typeof(Byte) ||
-           type == typeof(Char) ||
+        if(type == typeof(Byte)    ||
+           type == typeof(Char)    ||
            type == typeof(Boolean) ||
-           type == typeof(Int16) ||
-           type == typeof(Int32) ||
-           type == typeof(Int64) ||
-           type == typeof(UInt16) ||
-           type == typeof(UInt32) ||
-           type == typeof(UInt64) ||
-           type == typeof(Single) ||
+           type == typeof(Int16)   ||
+           type == typeof(Int32)   ||
+           type == typeof(Int64)   ||
+           type == typeof(UInt16)  ||
+           type == typeof(UInt32)  ||
+           type == typeof(UInt64)  ||
+           type == typeof(Single)  ||
            type == typeof(Double))
             return true;
-
         return false;
     }
 }
